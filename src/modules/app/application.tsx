d6aa6bfc6c84d436e99c5aa9b1bb3d22b67b4089
Nadeem Ahmed <nadahmed03@hotmail.com>
2025-02-04 15:25:58 +0100

    Show more layers

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -7,9 +7,27 @@ import { useGeographic } from "ol/proj";
 // Styling of OpenLayers components like zoom and pan controls
 import "ol/ol.css";
 
+import { GeoJSON } from "ol/format";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+
 // By calling the "useGeographic" function in OpenLayers, we tell that we want coordinates to be in degrees
 //  instead of meters, which is the default. Without this `center: [10.6, 59.9]` brings us to "null island"
 useGeographic();
+
+const osmLayer = new TileLayer({ source: new OSM() });
+const municipalityLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/geojson/kommuner.geojson",
+    format: new GeoJSON(),
+  }),
+});
+const schoolLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/geojson/skoler.geojson",
+    format: new GeoJSON(),
+  }),
+});
 
 // Here we create a Map object. Make sure you `import { Map } from "ol"`. Otherwise, the standard Javascript
 //  map data structure will be used
@@ -18,7 +36,7 @@ const map = new Map({
   //   with a certain zoom level
   view: new View({ center: [10.8, 59.9], zoom: 13 }),
   // map tile images will be from the Open Street Map (OSM) tile layer
-  layers: [new TileLayer({ source: new OSM() })],
+  layers: [osmLayer, municipalityLayer, schoolLayer],
 });
 
 // A functional React component
