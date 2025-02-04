40ec071b7c2f6b977c27cee5083125a91012eadb
Nadeem Ahmed <nadahmed03@hotmail.com>
2025-02-04 14:56:03 +0100

    Show map

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Application } from "./modules/app/application";
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  createRoot(rootElement).render(<Application />);
+} else {
+  console.error("'root' not found");
+}
